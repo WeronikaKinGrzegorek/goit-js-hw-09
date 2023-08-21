e5d1e7e6028bafb5244603d3f4bba554e7d5d7f8
WeronikaKinGrzegorek <126391598+WeronikaKinGrzegorek@users.noreply.github.com>
2023-08-21 18:17:24 +0000

    Deploying to gh-pages from @ WeronikaKinGrzegorek/goit-js-hw-09@1a1662a2d572c95b312e6af3f1f89aaef4175bc4 🚀

---
+++ 01-color-switcher.4e2b5a5f.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),o=document.querySelector("body");e.addEventListener("click",(function(){t=setInterval((function(){o.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),e.setAttribute("disabled",!0)}));n.addEventListener("click",(function(){clearInterval(t),e.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.4e2b5a5f.js.map
---
+++ 01-color-switcher.ea4a296c.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");let r;t.addEventListener("click",(()=>{r=setInterval((function(){o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.setAttribute("disabled",!0)}));e.addEventListener("click",(()=>{clearInterval(r),t.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.ea4a296c.js.map
