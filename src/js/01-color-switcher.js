1a1662a2d572c95b312e6af3f1f89aaef4175bc4
WeronikaKinGrzegorek <126391598+WeronikaKinGrzegorek@users.noreply.github.com>
2023-08-21 20:16:55 +0200

    done task 1 - color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+const documentBody = document.querySelector('body');
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+let intervalId;
+const handleStart = () => {
+  function changeColor() {
+    documentBody.style.backgroundColor = getRandomHexColor();
+  }
+  intervalId = setInterval(changeColor, 1000);
+  startButton.setAttribute('disabled', true);
+};
+
+startButton.addEventListener('click', handleStart);
+
+const handleStop = () => {
+  clearInterval(intervalId);
+  startButton.removeAttribute('disabled');
+};
+
+stopButton.addEventListener('click', handleStop);
